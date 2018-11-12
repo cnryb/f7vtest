63200a9ea13b6bdea5eaf802a157bd70398bd56c
任跃兵 <cnryb@live.cn>
2018-11-12 22:24:10 +0800

    双击返回键退出应用程序 处理Cordova事件

--- src/main.js
+++ src/main.js
@@ -10,4 +10,57 @@ export default new Vue({
   // Root Element
   el: '#app',
   render: c => c(App),
+  mounted() {
+    document.addEventListener("backbutton", this.onBackKeyDown, false);
+  },
+  methods: {
+    onBackKeyDown: function (e) {
+      e.preventDefault();
+      debugger;
+      if (this.$f7.view.current.history.length > 1) {
+        this.$f7.view.current.router.back();
+      } else {
+        const self = this;
+        // Create toast
+        if (!self.toastCenter) {
+          self.toastCenter = self.$f7.toast.create({
+            text: "连续点击退出应用程序",
+            position: "center",
+            closeTimeout: 1000
+          });
+        }
+        // Open it
+        self.toastCenter.open();
+
+        document.removeEventListener("backbutton", self.onBackKeyDown, false); //注销返回键 //1秒后重新注册
+        var intervalID = window.setInterval(function () {
+          window.clearInterval(intervalID);
+          document.addEventListener("backbutton", self.onBackKeyDown, false); //返回键
+        }, 1000);
+      }
+    }
+  },
 });
+
+
+var app = {
+  // Application Constructor
+  initialize: function () {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function () {
+    this.receivedEvent('deviceready');
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+    console.log('Received Event: ' + id);
+  }
+};
+
+app.initialize();
